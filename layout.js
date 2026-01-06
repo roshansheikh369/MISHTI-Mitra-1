be22d2f12729e8adc92e8f91ede3ee70000e16e4
roshansheikh369 <roshansheikh369@gmail.com>
2026-01-06 20:53:37 +0530

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'mishTee Delivery Mitra',
+  description: 'Delivery Agent Dashboard',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  )
+}
